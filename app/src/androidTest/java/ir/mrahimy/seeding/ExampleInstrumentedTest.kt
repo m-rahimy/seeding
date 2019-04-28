a8a21659bc37badec1313d66060fd040c01fec7c
mojtaba <mojtaba.rahimy@chmail.ir>
2019-04-28 16:23:25 +0430

    konverted test

---
+++ app/src/androidTest/java/ir/mrahimy/seeding/ExampleInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package ir.mrahimy.seeding
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("ir.mrahimy.seedbandi", appContext.packageName)
+    }
+
+    /* @Test
+    public void a(){
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        val fDbManager(appContext)
+    } */
+}
